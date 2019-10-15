77f5bf10fda7cda9d80095b6a05c706c8b46177f
Karim Slimani <karim@playmedia.fr>
2019-10-15 13:13:21 +0200

    [0.1.0]

--- dist/live-program-mediacontrol.js
+++ dist/live-program-mediacontrol.js
@@ -7,7 +7,7 @@
 		exports["LiveProgramMediaControl"] = factory(require("clappr"));
 	else
 		root["LiveProgramMediaControl"] = factory(root["Clappr"]);
-})(window, function(__WEBPACK_EXTERNAL_MODULE__1__) {
+})(window, function(__WEBPACK_EXTERNAL_MODULE__2__) {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -99,18 +99,18 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-
+/* WEBPACK VAR INJECTION */(function(process) {
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = void 0;
+exports["default"] = void 0;
 
-var _clappr = __webpack_require__(1);
+var _clappr = __webpack_require__(2);
 
-var _styles = _interopRequireDefault(__webpack_require__(2));
+var _styles = _interopRequireDefault(__webpack_require__(3));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
@@ -145,12 +145,12 @@ var LiveProgramMediaControl =
 function (_MediaControl) {
   _inherits(LiveProgramMediaControl, _MediaControl);
 
-  function LiveProgramMediaControl(options) {
+  function LiveProgramMediaControl(core) {
     var _this;
 
     _classCallCheck(this, LiveProgramMediaControl);
 
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(LiveProgramMediaControl).call(this, options));
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(LiveProgramMediaControl).call(this, core));
     _this._seekBarHidden = false;
     _this._setSeekBarHidden = !_clappr.Browser.isiOS; // Fix seekbar scrubber on iPhone
 
@@ -158,7 +158,7 @@ function (_MediaControl) {
     _this._rightPanelWidth = 0;
     _this._liveProgramLeft = 0;
     _this._liveProgramWidth = 0;
-    var pluginOpts = options.liveProgramMediaControl;
+    var pluginOpts = _this.options.liveProgramMediaControl;
 
     _this._setProgram(pluginOpts.program);
 
@@ -246,7 +246,7 @@ function (_MediaControl) {
       this.$liveProgram.append(this.$liveProgramBar);
       this.$liveProgram.append(this.$endAt);
       this.$el.find('.media-control-center-panel[data-media-control]').append(this.$liveProgram);
-      this.$el.append(_clappr.Styler.getStyleFor(_styles.default));
+      this.$el.append(_clappr.Styler.getStyleFor(_styles["default"]));
       this.$leftPanel = this.$el.find('.media-control-left-panel[data-media-control]');
       this.$rightPanel = this.$el.find('.media-control-right-panel[data-media-control]');
       this.updateLiveProgram();
@@ -269,7 +269,7 @@ function (_MediaControl) {
   }, {
     key: "updateLiveProgram",
     value: function updateLiveProgram() {
-      if (!this.$liveProgram) {
+      if (!this.$liveProgram || !this._program) {
         return;
       }
 
@@ -285,6 +285,22 @@ function (_MediaControl) {
       } else {
         this.$liveProgram.show();
       }
+    } // @override
+
+  }, {
+    key: "bindEvents",
+    value: function bindEvents() {
+      var _this2 = this;
+
+      _get(_getPrototypeOf(LiveProgramMediaControl.prototype), "bindEvents", this).call(this);
+
+      this.listenTo(this.core, _clappr.Events.CORE_FULLSCREEN, function (evt) {
+        // Ensure resizeLiveProgram() is called after browser entered
+        // or exited fullscreen mode
+        process.nextTick(function () {
+          _this2.resizeLiveProgram();
+        });
+      });
     } // @override
 
   }, {
@@ -364,27 +380,217 @@ function (_MediaControl) {
   return LiveProgramMediaControl;
 }(_clappr.MediaControl);
 
-exports.default = LiveProgramMediaControl;
+exports["default"] = LiveProgramMediaControl;
 module.exports = exports.default;
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(1)))
 
 /***/ }),
 /* 1 */
 /***/ (function(module, exports) {
 
-module.exports = __WEBPACK_EXTERNAL_MODULE__1__;
+// shim for using process in browser
+var process = module.exports = {};
+
+// cached from whatever global is present so that test runners that stub it
+// don't break things.  But we need to wrap it in a try catch in case it is
+// wrapped in strict mode code which doesn't define any globals.  It's inside a
+// function because try/catches deoptimize in certain engines.
+
+var cachedSetTimeout;
+var cachedClearTimeout;
+
+function defaultSetTimout() {
+    throw new Error('setTimeout has not been defined');
+}
+function defaultClearTimeout () {
+    throw new Error('clearTimeout has not been defined');
+}
+(function () {
+    try {
+        if (typeof setTimeout === 'function') {
+            cachedSetTimeout = setTimeout;
+        } else {
+            cachedSetTimeout = defaultSetTimout;
+        }
+    } catch (e) {
+        cachedSetTimeout = defaultSetTimout;
+    }
+    try {
+        if (typeof clearTimeout === 'function') {
+            cachedClearTimeout = clearTimeout;
+        } else {
+            cachedClearTimeout = defaultClearTimeout;
+        }
+    } catch (e) {
+        cachedClearTimeout = defaultClearTimeout;
+    }
+} ())
+function runTimeout(fun) {
+    if (cachedSetTimeout === setTimeout) {
+        //normal enviroments in sane situations
+        return setTimeout(fun, 0);
+    }
+    // if setTimeout wasn't available but was latter defined
+    if ((cachedSetTimeout === defaultSetTimout || !cachedSetTimeout) && setTimeout) {
+        cachedSetTimeout = setTimeout;
+        return setTimeout(fun, 0);
+    }
+    try {
+        // when when somebody has screwed with setTimeout but no I.E. maddness
+        return cachedSetTimeout(fun, 0);
+    } catch(e){
+        try {
+            // When we are in I.E. but the script has been evaled so I.E. doesn't trust the global object when called normally
+            return cachedSetTimeout.call(null, fun, 0);
+        } catch(e){
+            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error
+            return cachedSetTimeout.call(this, fun, 0);
+        }
+    }
+
+
+}
+function runClearTimeout(marker) {
+    if (cachedClearTimeout === clearTimeout) {
+        //normal enviroments in sane situations
+        return clearTimeout(marker);
+    }
+    // if clearTimeout wasn't available but was latter defined
+    if ((cachedClearTimeout === defaultClearTimeout || !cachedClearTimeout) && clearTimeout) {
+        cachedClearTimeout = clearTimeout;
+        return clearTimeout(marker);
+    }
+    try {
+        // when when somebody has screwed with setTimeout but no I.E. maddness
+        return cachedClearTimeout(marker);
+    } catch (e){
+        try {
+            // When we are in I.E. but the script has been evaled so I.E. doesn't  trust the global object when called normally
+            return cachedClearTimeout.call(null, marker);
+        } catch (e){
+            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error.
+            // Some versions of I.E. have different rules for clearTimeout vs setTimeout
+            return cachedClearTimeout.call(this, marker);
+        }
+    }
+
+
+
+}
+var queue = [];
+var draining = false;
+var currentQueue;
+var queueIndex = -1;
+
+function cleanUpNextTick() {
+    if (!draining || !currentQueue) {
+        return;
+    }
+    draining = false;
+    if (currentQueue.length) {
+        queue = currentQueue.concat(queue);
+    } else {
+        queueIndex = -1;
+    }
+    if (queue.length) {
+        drainQueue();
+    }
+}
+
+function drainQueue() {
+    if (draining) {
+        return;
+    }
+    var timeout = runTimeout(cleanUpNextTick);
+    draining = true;
+
+    var len = queue.length;
+    while(len) {
+        currentQueue = queue;
+        queue = [];
+        while (++queueIndex < len) {
+            if (currentQueue) {
+                currentQueue[queueIndex].run();
+            }
+        }
+        queueIndex = -1;
+        len = queue.length;
+    }
+    currentQueue = null;
+    draining = false;
+    runClearTimeout(timeout);
+}
+
+process.nextTick = function (fun) {
+    var args = new Array(arguments.length - 1);
+    if (arguments.length > 1) {
+        for (var i = 1; i < arguments.length; i++) {
+            args[i - 1] = arguments[i];
+        }
+    }
+    queue.push(new Item(fun, args));
+    if (queue.length === 1 && !draining) {
+        runTimeout(drainQueue);
+    }
+};
+
+// v8 likes predictible objects
+function Item(fun, array) {
+    this.fun = fun;
+    this.array = array;
+}
+Item.prototype.run = function () {
+    this.fun.apply(null, this.array);
+};
+process.title = 'browser';
+process.browser = true;
+process.env = {};
+process.argv = [];
+process.version = ''; // empty string to avoid regexp issues
+process.versions = {};
+
+function noop() {}
+
+process.on = noop;
+process.addListener = noop;
+process.once = noop;
+process.off = noop;
+process.removeListener = noop;
+process.removeAllListeners = noop;
+process.emit = noop;
+process.prependListener = noop;
+process.prependOnceListener = noop;
+
+process.listeners = function (name) { return [] }
+
+process.binding = function (name) {
+    throw new Error('process.binding is not supported');
+};
+
+process.cwd = function () { return '/' };
+process.chdir = function (dir) {
+    throw new Error('process.chdir is not supported');
+};
+process.umask = function() { return 0; };
+
 
 /***/ }),
 /* 2 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-exports = module.exports = __webpack_require__(3)(false);
-// Module
-exports.push([module.i, ".live-program[data-media-control] {\n  position: absolute;\n  left: 100px;\n  bottom: 8px;\n  display: inline-block;\n  width: 50%;\n  height: 16px; }\n  .live-program[data-media-control] .live-program-start[data-media-control] {\n    position: absolute;\n    left: 0px;\n    top: 0px;\n    width: 40px;\n    height: 100%;\n    font-size: 10px;\n    color: #fff;\n    text-align: center;\n    line-height: 16px; }\n  .live-program[data-media-control] .live-program-end[data-media-control] {\n    position: absolute;\n    right: 0px;\n    top: 0px;\n    width: 40px;\n    height: 100%;\n    font-size: 10px;\n    color: #fff;\n    text-align: center;\n    line-height: 16px; }\n  .live-program[data-media-control] .live-program-bar[data-media-control] {\n    position: absolute;\n    top: 0px;\n    left: 0px;\n    width: 100%;\n    height: 100%;\n    padding: 5px 40px; }\n    .live-program[data-media-control] .live-program-bar[data-media-control] .live-program-progress-bar[data-media-control] {\n      position: relative;\n      width: 100%;\n      height: 100%; }\n      .live-program[data-media-control] .live-program-bar[data-media-control] .live-program-progress-bar[data-media-control] .live-program-progress-bar-bg[data-media-control] {\n        position: absolute;\n        top: 0;\n        left: 0;\n        width: 100%;\n        height: 6px;\n        background-color: #c2c2c2; }\n      .live-program[data-media-control] .live-program-bar[data-media-control] .live-program-progress-bar[data-media-control] .live-program-progress-bar-fill[data-media-control] {\n        position: absolute;\n        top: 0;\n        left: 0;\n        width: 0;\n        height: 6px;\n        background-color: #ff0101;\n        -webkit-transition: all 0.5s ease-out;\n        -moz-transition: all 0.5s ease-out;\n        -o-transition: all 0.5s ease-out;\n        transition: all 0.5s ease-out; }\n", ""]);
-
-
+module.exports = __WEBPACK_EXTERNAL_MODULE__2__;
 
 /***/ }),
 /* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(4)(false);
+// Module
+exports.push([module.i, ".live-program[data-media-control]{position:absolute;left:100px;bottom:8px;display:inline-block;width:50%;height:16px}.live-program[data-media-control] .live-program-start[data-media-control]{position:absolute;left:0px;top:0px;width:40px;height:100%;font-size:10px;color:#fff;text-align:center;line-height:16px}.live-program[data-media-control] .live-program-end[data-media-control]{position:absolute;right:0px;top:0px;width:40px;height:100%;font-size:10px;color:#fff;text-align:center;line-height:16px}.live-program[data-media-control] .live-program-bar[data-media-control]{position:absolute;top:0px;left:0px;width:100%;height:100%;padding:5px 40px}.live-program[data-media-control] .live-program-bar[data-media-control] .live-program-progress-bar[data-media-control]{position:relative;width:100%;height:100%}.live-program[data-media-control] .live-program-bar[data-media-control] .live-program-progress-bar[data-media-control] .live-program-progress-bar-bg[data-media-control]{position:absolute;top:0;left:0;width:100%;height:6px;background-color:#c2c2c2}.live-program[data-media-control] .live-program-bar[data-media-control] .live-program-progress-bar[data-media-control] .live-program-progress-bar-fill[data-media-control]{position:absolute;top:0;left:0;width:0;height:6px;background-color:#ff0101;-webkit-transition:all .5s ease-out;-moz-transition:all .5s ease-out;-o-transition:all .5s ease-out;transition:all .5s ease-out}\n", ""]);
+
+
+/***/ }),
+/* 4 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -395,6 +601,7 @@ exports.push([module.i, ".live-program[data-media-control] {\n  position: absolu
   Author Tobias Koppers @sokra
 */
 // css base code, injected by the css-loader
+// eslint-disable-next-line func-names
 module.exports = function (useSourceMap) {
   var list = []; // return the list of modules as css string
 
@@ -403,22 +610,25 @@ module.exports = function (useSourceMap) {
       var content = cssWithMappingToString(item, useSourceMap);
 
       if (item[2]) {
-        return '@media ' + item[2] + '{' + content + '}';
-      } else {
-        return content;
+        return "@media ".concat(item[2], "{").concat(content, "}");
       }
+
+      return content;
     }).join('');
   }; // import a list of modules into the list
+  // eslint-disable-next-line func-names
 
 
   list.i = function (modules, mediaQuery) {
     if (typeof modules === 'string') {
+      // eslint-disable-next-line no-param-reassign
       modules = [[null, modules, '']];
     }
 
     var alreadyImportedModules = {};
 
     for (var i = 0; i < this.length; i++) {
+      // eslint-disable-next-line prefer-destructuring
       var id = this[i][0];
 
       if (id != null) {
@@ -426,8 +636,8 @@ module.exports = function (useSourceMap) {
       }
     }
 
-    for (i = 0; i < modules.length; i++) {
-      var item = modules[i]; // skip already imported module
+    for (var _i = 0; _i < modules.length; _i++) {
+      var item = modules[_i]; // skip already imported module
       // this implementation is not 100% perfect for weird media query combinations
       // when a module is imported multiple times with different media queries.
       // I hope this will never occur (Hey this way we have smaller bundles)
@@ -436,7 +646,7 @@ module.exports = function (useSourceMap) {
         if (mediaQuery && !item[2]) {
           item[2] = mediaQuery;
         } else if (mediaQuery) {
-          item[2] = '(' + item[2] + ') and (' + mediaQuery + ')';
+          item[2] = "(".concat(item[2], ") and (").concat(mediaQuery, ")");
         }
 
         list.push(item);
@@ -448,7 +658,8 @@ module.exports = function (useSourceMap) {
 };
 
 function cssWithMappingToString(item, useSourceMap) {
-  var content = item[1] || '';
+  var content = item[1] || ''; // eslint-disable-next-line prefer-destructuring
+
   var cssMapping = item[3];
 
   if (!cssMapping) {
@@ -458,7 +669,7 @@ function cssWithMappingToString(item, useSourceMap) {
   if (useSourceMap && typeof btoa === 'function') {
     var sourceMapping = toComment(cssMapping);
     var sourceURLs = cssMapping.sources.map(function (source) {
-      return '/*# sourceURL=' + cssMapping.sourceRoot + source + ' */';
+      return "/*# sourceURL=".concat(cssMapping.sourceRoot).concat(source, " */");
     });
     return [content].concat(sourceURLs).concat([sourceMapping]).join('\n');
   }
@@ -470,8 +681,8 @@ function cssWithMappingToString(item, useSourceMap) {
 function toComment(sourceMap) {
   // eslint-disable-next-line no-undef
   var base64 = btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap))));
-  var data = 'sourceMappingURL=data:application/json;charset=utf-8;base64,' + base64;
-  return '/*# ' + data + ' */';
+  var data = "sourceMappingURL=data:application/json;charset=utf-8;base64,".concat(base64);
+  return "/*# ".concat(data, " */");
 }
 
 /***/ })
